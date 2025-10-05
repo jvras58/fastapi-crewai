1c1592150161399100fdcf050abf4110e2bdfb6b
jvras58 <jvras@cin.ufpe.br>
2025-10-05 03:58:57 -0300

    refatora: adiciona importações de modelos de IA e cria migrações para tabelas de documentos, conversas e mensagens

--- apps/ia/models/__init__.py
+++ apps/ia/models/__init__.py
@@ -9,3 +9,9 @@ Ref: https://stackoverflow.com/questions/9088957/sqlalchemy-cannot-find-a-class-
 from apps.ia.models.conversation import Conversation  # noqa F401
 from apps.ia.models.document import Document  # noqa F401
 from apps.ia.models.message import Message  # noqa F401
+
+from apps.core.models.assignment import Assignment  # noqa F401
+from apps.core.models.authorization import Authorization  # noqa F401
+from apps.core.models.role import Role  # noqa F401
+from apps.core.models.transaction import Transaction  # noqa F401
+from apps.core.models.user import User  # noqa F401
